170a1e523bf8762d091c2d25b2e45a84bb9992c9
AntonyM71 <42292224+AntonyM71@users.noreply.github.com>
2021-10-23 14:58:58 +0100

    add more pages for projects (#1)

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -4,9 +4,13 @@ import { useToasts } from "react-toast-notifications"
 import { registerRejectedPromise } from "../topLevelErrorHandler"
 import CV from "./cv"
 import Home from "./home"
+import Projects from "./projects"
 export const routes = {
     home: {
         root: "/",
+    },
+    projects: {
+        root: "/projects",
     },
     cv: {
         root: "/cv",
@@ -21,6 +25,7 @@ const Router = () => {
         <Switch>
             <Route path={routes.cv.root} component={CV} />
             <Route path={routes.home.root} component={Home} />
+            <Route path={routes.projects.root} component={Projects} />
         </Switch>
     )
 }
