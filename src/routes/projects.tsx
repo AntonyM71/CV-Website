97c509cd8d1bcbff3b90998f96f9481918a9ff47
Ant-Mac <antonymitchell@hotmail.com>
2021-10-23 20:58:48 +0100

    Add QR code

--- src/routes/projects.tsx
+++ src/routes/projects.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-const Home = () => <h1>Home Page</h1>
+const Projects = () => <h1>Projects Page</h1>
 
-export default Home
+export default Projects
 // https://levelup.gitconnected.com/displaying-pdf-in-react-app-6e9d1fffa1a9
